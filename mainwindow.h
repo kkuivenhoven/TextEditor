57087cbf2c848c8ccd2cd063f1f20bf8708cc3d0
Kendra <kkuivenhoven@mail.csuchico.edu>
2021-02-10 13:23:17 -0800

    Removed all slot functions from mainwindow class

--- mainwindow.h
+++ mainwindow.h
@@ -48,58 +48,13 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-    // Q_OBJECT
-
-    QTabBar *_tabBar;
-    int _tabCount;
-    QList<QString> _tabList;
-    QList<QAction*> _menuActions;
-    QTabWidget *_tabWidget;
-    QMenu *_optionsMenu;
-    QList<QWidget*> _windowList;
-    QList<QVBoxLayout*> _vBoxList;
-    int _windowCount;
-    QHash<int, QTextEdit*> _textEditPerWindow;
-    QHash<QWidget*, int> _totalParentWindows;
-    QHash<int, QLineEdit*> _searchBarRecord;
-    int _qMainWindowCount;
 
 public:
-    QHash<QWidget*, int> _totalWindows;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-protected:
-    // void closeEvent(QCloseEvent *event) override;
-
-private slots:
-    void on_saveFileButton_clicked();
-    void on_quitButton_clicked();
-    void on_textEdit_copyAvailable(bool b);
-    void on_clear_text_click();
-    void on_open_file_click();
-    void on_click_create_new_file();
-    void on_saveFileMenu_clicked();
-
-    void on_searchReplace_clicked();
-    void on_searchButton_clicked();
-    void on_clearSearch_clicked();
-    void on_wordCount_clicked();
-    void on_wordCountMenu_clicked();
-
-    void on_quitWindow_clicked();
-    void onClickCreate_newQMainWindow();
-    void onClickQMainWindowQuit();
-    void onClickWordCount();
-    void on_quitMenu_clicked();
-    void on_searchReplaceMenu();
-
-    void on_copy_text_click();
-
 private:
     Ui::MainWindow *ui;
-    QWidget* _getCurrentWindow(QAction *actionSender, QWidget *currentWindow);
-    bool closeWindowEvent();
 
 };
 
